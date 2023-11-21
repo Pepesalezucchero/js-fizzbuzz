31eb2d1f1b5369879475167604e83ea052dedec8
Alessio Rugi <alessiorugi95@gmail.com>
2023-11-21 17:10:14 +0100

    primo bonus fatto

---
+++ bonus/js/script.js
@@ -0,0 +1,40 @@
+/*Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz”, al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+//Primo bonus: Crea un container nel DOM , aggiungendo attraverso la funzione append() un elemento html con il numero o la stringa corretta da mostrare.
+
+const userContainer = document.querySelector(".container"); //bonus 1
+userSpan = document.createElement("span");
+userContainer.append(userSpan);
+
+//scrivo un programma che stampi i unemri da 1 a 100 in cosole.
+for (let i = 1; i <= 100; i++) {
+
+    //I multipli di 3 e di 5 avranno la scritta FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0) {
+        userSpan.append( "FizzBuzz");
+        console.log ("FizzBuzz");
+        
+    //I multipli di 3 avranno la scritta Fizz
+    } else if (i % 3 === 0) {
+        userSpan.append( "Fizz");
+        console.log ("Fizz");
+
+    //I multipli di 5 avranno la scritta Buzz
+    } else if (i % 5 === 0) {
+        userSpan.append( "Buzz");
+        console.log ("Buzz");
+
+    //tutti gli altri
+    } else {
+        userSpan.append( i);
+        console.log (i);
+    }
+
+    userSpan = document.querySelector("span"); //bonus 1
+    userBr = document.createElement("br");
+    userSpan.append(userBr);
+}
+
